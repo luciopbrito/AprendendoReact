bc08114eea5255ef2970777e84ddcb2cadc33828
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-02-06 17:05:53 -0300

    feat: aula 16: instrucao condicional versao 1;

--- react001/src/App.js
+++ react001/src/App.js
@@ -5,11 +5,15 @@ import ComponenteDois from "./components/ComponenteDois/ComponenteDois";
 import ComponenteTres from "./components/ComponenteTres/ComponenteTres";
 import ComponenteQuatro from "./components/ComponenteQuatro/ComponenteQuatro";
 import ComponenteSeis from "./components/ComponenteSeis/ComponenteSeis";
+import ComponenteSete from "./components/ComponenteSete/ComponenteSete";
 
 function App(){
     return (
         <div>            
+            <ComponenteSete titulo="Componente Sete" valor={0}/>
+
             <ComponenteSeis titulo="clientes"/>
+            
             <ComponenteQuatro titulo="Componente Quatro"/>             
 
             <ComponenteTres titulo="Componente Três" subtitulo="Texto do Componente Três"/>
