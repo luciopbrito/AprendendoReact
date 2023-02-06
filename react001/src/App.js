0f5b7df1a85be81ae6a61f2e95051cf9eae264ae
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-02-06 15:28:20 -0300

    feat: comentários jsx e props children;

--- react001/src/App.js
+++ react001/src/App.js
@@ -3,11 +3,19 @@ import "./index.css"
 import ComponenteUm from "./components/ComponenteUm/ComponenteUm";
 import ComponenteDois from "./components/ComponenteDois/ComponenteDois";
 import ComponenteTres from "./components/ComponenteTres/ComponenteTres";
+import ComponenteQuatro from "./components/ComponenteQuatro/ComponenteQuatro";
 
 function App(){
     return (
-        <div>
-            <ComponenteTres titulo="teste"/>
+        <div>            
+            <ComponenteQuatro titulo="Componente Quatro">
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                </ul>
+            </ComponenteQuatro>
+            <ComponenteTres titulo="Componente Três" subtitulo="Texto do Componente Três"/>
             <ComponenteDois />
             <ComponenteUm />
         </div>
--- react001/src/components/ComponenteTres/ComponenteTres.jsx
+++ react001/src/components/ComponenteTres/ComponenteTres.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export default props => 
-    <div className="componente">
-        <p>Título: {props.titulo}</p>
-    </div>
+const ComponenteTres = (props) => {
+
+    return (
+        <div className="componente">
+            <p>Titulo: {props.titulo}</p>
+            <p>Subtitulo: {props.subtitulo}</p>
+        </div>
+    )
+} 
+
+export default ComponenteTres;
