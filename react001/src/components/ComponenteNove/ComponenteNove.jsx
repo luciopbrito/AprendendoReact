7b5dc7827558ba00b59a40f7b273f2d7c9b932dc
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-02-08 18:14:22 -0300

    feat: aula19: aplicando style inline;

--- react001/src/components/ComponenteNove/ComponenteNove.jsx
+++ react001/src/components/ComponenteNove/ComponenteNove.jsx
@@ -7,15 +7,15 @@ export default function(props){
             <p className="titulo">Título: {props.titulo}</p>
             <p>Nome: {props.valor}</p>
 
-            <CondicaoIf condicao={props.valor == "joao"}>
+            <CondicaoIf condicao={props.valor === "joao"}>
                 <p>Verdadeiro, é o meu nome</p>
             </CondicaoIf>
         
-            <CondicaoIf condicao={props.valor == "ana"}>
+            <CondicaoIf condicao={props.valor === "ana"}>
                 <p>É a minha irmã</p>
             </CondicaoIf>
           
-            <CondicaoIf condicao={props.valor != "joao" && props.valor != "ana"}>
+            <CondicaoIf condicao={props.valor !== "joao" && props.valor !== "ana"}>
                 <p>Desconheço este nome</p>
             </CondicaoIf>
 
