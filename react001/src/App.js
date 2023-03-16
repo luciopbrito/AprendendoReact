04400ab36963913eaefeef4777ccb662ce5a8627
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-03-16 01:18:31 -0300

    feat: aula26: a forma correta de utilizar valores com useState;

--- react001/src/App.js
+++ react001/src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 export default function App(){
 
-    const [valor, setValor] = useState(10)
+    const [valor, setValor] = useState(100)
 
-    function decremento(){
-        setValor(valor - 1);
+    function diminuir(){
+        setValor(valor_antigo => valor_antigo - 1)
+        setValor(valor_antigo => valor_antigo - 1)
+        setValor(valor_antigo => valor_antigo - 1)
     }
 
-    function incremento(){ 
-        setValor(valor + 1);
+    function aumentar(){
+        setValor(valor_antigo => valor_antigo + 1)
+        setValor(valor_antigo => valor_antigo + 1)
+        setValor(valor_antigo => valor_antigo + 1)
     }
 
     return (
         <>
             <h1>React Hook</h1>
-            <p>Valor: {valor}</p>
-            <button onClick={decremento}>Decremento</button>
-            <button onClick={incremento}>Incremento</button>
+            <button onClick={diminuir}>Diminuir</button>
+            <span>Valor: {valor}</span> 
+            <button onClick={aumentar}>Aumentar</button>
         </>
     )
 }
